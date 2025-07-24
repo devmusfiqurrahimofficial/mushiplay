b6286b7fa66281add38e2188e10a6957e0102142
devmusfiqurrahinmofficial <devmusfiqurrahimofficial@gmail.com>
2025-07-25 01:31:32 +0600

    xc

--- script.js
+++ script.js
@@ -2,29 +2,30 @@ console.log("Welcome to Spotify");
 
 // Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('songs/1.mp3');
+let audioElement = new Audio('songs/Warfaze-Purnota.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
-
+    
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Purnota-Warfaze", filePath:"songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Tomake-Warfaze", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Protichobi-bagdhara", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Bhalobasha Baki-Popaye", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "DukhoBilash-Artcell", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Etota Bhalobashhi-Recall", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Aj rate kono rupkotha nei-Old school", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Kosto-AvoidRafa", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "17 pristha-Ashes", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Pakhi-Shironamhin", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=>{ 
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
-    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+ 
 })
  
 
@@ -106,4 +107,4 @@ document.getElementById('previous').addEventListener('click', ()=>{
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
-})
+})  
